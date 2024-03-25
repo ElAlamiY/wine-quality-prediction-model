e4c76004a9b112ac56dcb33687c0f05e00e5ee26
Yassine El Alami <y.elalamiy@gmail.com>
2024-03-25 02:01:18 +0100

    gbmmodel pipeline

--- models/gbmmodel.py
+++ models/gbmmodel.py
@@ -12,7 +12,7 @@ def xgbmodel(data):
 
     # Split the data into training, validation, and testing sets
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.4, random_state=42)
 
     # Scale the features
     scaler = StandardScaler()
